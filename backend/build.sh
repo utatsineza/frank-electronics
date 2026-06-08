8954c3f17d2d0edc73ad2ca6b0c6b27136d34c82
kigali kigali <utatsinezahenriette@gmail.com>
2026-06-08 13:20:07 +0200

    ok

--- backend/build.sh
+++ backend/build.sh
@@ -3,3 +3,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from users.models import User
+if not User.objects.filter(email='admin@frankelectronics.rw').exists():
+    User.objects.create_superuser(email='admin@frankelectronics.rw', name='Admin', password='admin2026')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
