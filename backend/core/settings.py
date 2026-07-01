dbd0c5b608bcf3872b49ff567972ecc54d851c78
kigali kigali <utatsinezahenriette@gmail.com>
2026-07-01 22:25:41 +0200

    updating settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -116,7 +116,7 @@ EMAIL_PORT       = int(os.getenv('EMAIL_PORT', 465))
 EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS    = True
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@frankelectronics.rw')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'utatsinezahenriette@gmail.com')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
